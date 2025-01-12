4d76b0e3c3722870c4844acbfc524ca92282f719
Melinda Golden <info@melindagolden.com>
2025-01-11 20:57:56 -0500

    feat: Install and configure PostCSS and Stylelint

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,34 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+import postcssNested from "postcss-nested";
+import autoprefixer from "autoprefixer";
+import postcssSimpleVars from "postcss-simple-vars";
+import { formatPrimeVueVariables } from "./src/utils/formatPrimeVueVariables";
+// @ts-expect-error no declaration file
+import base from "@primevue/themes/lara/base";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  plugins: [vue(), vueDevTools()],
+
+  css: {
+    postcss: {
+      plugins: [
+        postcssSimpleVars({
+          variables: formatPrimeVueVariables(base.primitive),
+        }),
+        postcssNested(),
+        autoprefixer(),
+      ],
     },
   },
-})
+
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
